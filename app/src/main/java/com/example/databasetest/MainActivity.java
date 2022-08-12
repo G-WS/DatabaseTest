80354491ab7052fa43431f5c1bf9cada9333afea
G-WS <3355139577@qq.com>
2022-08-12 12:52:49 +0800

    0812

--- app/src/main/java/com/example/databasetest/MainActivity.java
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.databasetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private  MydatabaseHelper dbhelper;
+private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbhelper = new MydatabaseHelper(MainActivity.this,"BookStore.db",null,2);
+        Button create = (Button) findViewById(R.id.create_database);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbhelper.getWritableDatabase();
+
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase database = dbhelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+
+                values.put("name","The da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.96);
+
+                database.insert("Book",null,values);
+
+                values.clear();
+
+                values.put("name","The Lost Symbol");
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",19.95);
+
+                database.insert("Book",null,values);
+            }
+        });
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbhelper.getWritableDatabase();
+                db.delete("Book","pages > ?",new String[]{"500"});
+            }
+        });
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbhelper.getWritableDatabase();
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if (cursor.moveToFirst()){
+                    do {
+                        @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex("name"));
+                        @SuppressLint("Range") String author = cursor.getString(cursor.getColumnIndex("author"));
+                        @SuppressLint("Range") int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        @SuppressLint("Range") double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d(TAG, "Book name is"+name);
+                        Log.d(TAG, "Book author is"+author);
+                        Log.d(TAG, "Book pages is"+pages);
+                        Log.d(TAG, "Book price is"+price);
+
+
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/databasetest/MydatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MydatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_BOOK = "create table Book("
+            + "id integer primary key autoincrement,"
+            + "author text,"
+            + "price real,"
+            + "pages integer,"
+            + "name text)";
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement,"
+            + "category_name text,"
+            + "category_code integer)";
+    private Context mcontext;
+
+    public MydatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mcontext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mcontext, "create succeed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Category");
+        onCreate(sqLiteDatabase);
+    }
+}
